ba975397a55c1462d9e950e1ae73068c05bbb717
sonnythehottest <sonthehottest@gmail.com>
2014-06-05 02:05:58 +0700

    Added Transaction Request

--- bayar.php
+++ bayar.php
@@ -1,14 +1,19 @@
 <?php
 
-$req = json_encode(array("payment_type" => "vtweb"));
-$username = "6d7ccd71-ea52-43cc-ac42-5402077bd6c6";
+$req = new stdClass;
+$req->payment_type = "vtweb";
+$req->vtweb = (object) array("enabled_payments" => ["credit_card"]);
+$req->transaction_details = (object) array("order_id" => "order1", "gross_amount" => $_POST["price"]);
+$req->item_details = [(object) array("id" => $_POST["itemid"], "price" => $_POST["price"], "quantity" => $_POST["quantity"], "name" => $_POST["name"])];
+$req->customer_details = (object) array("first_name" => $_POST["fname"], "last_name" => $_POST["lname"], "email" => $_POST["mail"], "phone" => $_POST["phone"], "billing_address" => (object) array("first_name" => $_POST["bifname"], "last_name" => $_POST["bilname"], "address" => $_POST["biaddress"], "city" => $_POST["bicity"], "postal_code" => $_POST["bipostcode"], "phone" => $_POST["biphone"], "country_code" => $_POST["biccode"]), "shipping_address" => (object) array("first_name" => $_POST["shfname"], "last_name" => $_POST["shlname"], "address" => $_POST["shaddress"], "city" => $_POST["shcity"], "postal_code" => $_POST["shpostcode"], "phone" => $_POST["shphone"], "country_code" => $_POST["shccode"]));
 
-$string = file_get_contents("/var/www/veritrans/data.json");
+$username = "6d7ccd71-ea52-43cc-ac42-5402077bd6c6";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.sandbox.veritrans.co.id/v2/charge");
+curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $string);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($req));
 curl_setopt($ch, CURLOPT_USERPWD, $username);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 	'Accept: application/json',
@@ -17,6 +22,10 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 ));
 
 $result = curl_exec($ch);
-echo $result;
+curl_close($ch);
+
+$hasil = json_decode($result, true);
+var_dump($result);
+var_dump($hasil);
 
 ?>
--- index.php
+++ index.php
@@ -2,39 +2,32 @@
 	<h1>Welcome to Veritrans</h1>
 
 	<form method="post" action="bayar.php">
-		<fieldset>
 			<legend>Item Details</legend>
-				<input type="text" placeholder="id">
-				<input name="price" type="text" placeholder="price">
-				<input name="quantity" type="text" placeholder="quantity">
-				<input name="name" type="text" placeholder="name">
-		</fieldset>
-		<fieldset>
+			<input name="itemid" type="text" placeholder="id">
+			<input name="price" type="text" placeholder="price">
+			<input name="quantity" type="text" placeholder="quantity">
+			<input name="name" type="text" placeholder="name">
 			<legend>Customer Details</legend>
-			<input type="text" placeholder="First Name"></input>
-			<input type="text" placeholder="Last Name"></input>
-			<input type="text" placeholder="Email"></input>
-			<input type="text" placeholder="Phone"></input>
-		</fieldset>
-		<fieldset>
+			<input name="fname" type="text" placeholder="First Name"></input>
+			<input name="lname" type="text" placeholder="Last Name"></input>
+			<input name="mail" type="text" placeholder="Email"></input>
+			<input name="phone" type="text" placeholder="Phone"></input>
 			<legend>Billing Address</legend>
-			<input type="text" placeholder="First Name"></input>
-			<input type="text" placeholder="Last Name"></input>
-			<input type="text" placeholder="Address"></input>
-			<input type="text" placeholder="City"></input>
-			<input type="text" placeholder="Phone"></input>
-			<input type="text" placeholder="Country Code"></input>
-		</fieldset>
-		<fieldset>
+			<input name="bifname" type="text" placeholder="First Name"></input>
+			<input name="bilname" type="text" placeholder="Last Name"></input>
+			<input name="biaddress" type="text" placeholder="Address"></input>
+			<input name="bicity" type="text" placeholder="City"></input>
+			<input name="bipostcode" type="text" placeholder="Postal Code"></input>
+			<input name="biphone" type="text" placeholder="Phone"></input>
+			<input name="biccode" type="text" placeholder="Country Code"></input>
 			<legend>Shipping Address</legend>
-			<input type="text" placeholder="First Name"></input>
-			<input type="text" placeholder="Last Name"></input>
-			<input type="text" placeholder="Address"></input>
-			<input type="text" placeholder="City"></input>
-			<input type="text" placeholder="Postal Code"></input>
-			<input type="text" placeholder="Phone"></input>
-			<input type="text" placeholder="Country Code"></input>
-		</fieldset>
+			<input name="shfname" type="text" placeholder="First Name"></input>
+			<input name="snlname" type="text" placeholder="Last Name"></input>
+			<input name="shaddress" type="text" placeholder="Address"></input>
+			<input name="shcity" type="text" placeholder="City"></input>
+			<input name="shpostcode" type="text" placeholder="Postal Code"></input>
+			<input name="shphone" type="text" placeholder="Phone"></input>
+			<input name="shccode" type="text" placeholder="Country Code"></input>
 		<input type="submit" value="beli sekarang">
 	</form>
 </html>
